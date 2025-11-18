974136ec2a1352fc68cc66b0ceec6ef97f67b372
torvista <4391026+torvista@users.noreply.github.com>
2025-11-18 14:22:51 +0100

    add parent references to installer.

--- files/zc_plugins/ImageChecker/v2.3.0/Installer/ScriptedInstaller.php
+++ files/zc_plugins/ImageChecker/v2.3.0/Installer/ScriptedInstaller.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Plugin: Image Checker
  * @link https://github.com/torvista/Zen_Cart-Image_Checker
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @updated 03 August 2025 torvista
+ * @updated 22 August 2025 torvista
  */
 
 use Zencart\PluginSupport\ScriptedInstaller as ScriptedInstallBase;
@@ -17,10 +17,14 @@ class ScriptedInstaller extends ScriptedInstallBase
     {
         zen_deregister_admin_pages(['toolsImageChecker']);
         zen_register_admin_page('toolsImageChecker', 'BOX_TOOLS_IMAGE_CHECKER', 'FILENAME_IMAGE_CHECKER', '', 'tools', 'Y');
+
+        parent::executeInstall();
     }
 
     protected function executeUninstall()
     {
         zen_deregister_admin_pages(['toolsImageChecker']);
+
+        parent::executeUninstall();
     }
 }
